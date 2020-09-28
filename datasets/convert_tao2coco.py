50ceb51053efbb5002026ec971779a58d8b9338c
ga27kuh <ga27kuh@mytum.de>
2020-09-28 13:20:19 +0200

    tbc

--- datasets/convert_tao2coco.py
+++ datasets/convert_tao2coco.py
@@ -1,15 +1,52 @@
 import json
 
 
+def coco_id2tao_id():
+    # Loading json files
+    val_annot_path = "/home/kloping/OpenSet_MOT/data/TAO/annotations/validation.json"
+    with open(val_annot_path, 'r') as f:
+        val_annot_dict = json.load(f)
+
+    with open("coco/coco2synset.json") as f:
+        coco2synset = json.load(f)
+
+    # list all the categories of TAO (in the style of synset)
+    TAO_categories = list()
+    TAO_ids = list()
+
+    for c in val_annot_dict['categories']:
+        id = c['id']
+        TAO_ids.append(c['id'])
+        TAO_categories.append(c['synset'])
+
+    synset_cls = [v['synset'] for k, v in coco2synset.items()]
+    coco_ids = [v['coco_cat_id'] for k, v in coco2synset.items()]
+
+    coco2tao = dict()
+    coco_leftout = list()
+    for s, coco_id in zip(synset_cls, coco_ids):
+        if s in TAO_categories:
+            i = TAO_categories.index(s)
+            coco2tao[coco_id] = TAO_ids[i]
+        else:
+            coco_leftout.append((coco_id, s))
+
+    print("The following synset categories are left out")
+    print(coco_leftout)
+
+    return coco2tao
+
+
+
+
 def check_coco_tao_intersection(tao_category_dict, split='TRAIN'):
     # list all the categories of TAO
-    TAO_categories = set()
+    TAO_categories = list()
     for c in tao_category_dict:
-        class_name = c['name']
-        TAO_categories.add(class_name)
+        # class_name = c['name']
+        class_name = c['synset']
+        TAO_categories.append(class_name)
 
-    TAO_categories = list(TAO_categories)
-    TAO_categories.sort()
 
     # list all the categories of COCO
     with open('coco/coco_classes.json', 'r') as f:
@@ -25,12 +62,12 @@ def check_coco_tao_intersection(tao_category_dict, split='TRAIN'):
 
     # synset_cls = [v['synset'][:-5] for k, v in coco2synset.items()]
     synset_cls = [v['synset'] for k, v in coco2synset.items()]
-    synset_cls.sort()
+    coco_ids = [v['coco_cat_id'] for k, v in coco2synset.items()]
 
     TAO_categories_string = " ".join(TAO_categories)
     for coco_c in synset_cls:
-        # if coco_c not in TAO_categories:
-        if coco_c not in TAO_categories_string:
+        if coco_c not in TAO_categories:
+        # if coco_c not in TAO_categories_string:
             coco_leftout.append(coco_c)
 
     print("Number of COCO categories that are not covered in the TAO_{}: ".format(split), len(coco_leftout))
@@ -87,14 +124,16 @@ def check_train_val_diff(train_dict, val_dict):
         else:
             unknowns.append(val_cls)
 
-    print("Number of knowns:", len(knowns))
-    print(knowns)
-    print("Number of unknowns:", len(unknowns))
-    print(unknowns)
+    # print("Number of knowns:", len(knowns))
+    # print(knowns)
+    # print("Number of unknowns:", len(unknowns))
+    # print(unknowns)
 
 
 if __name__ == "__main__":
+    coco_id2tao_id()
 
+    # The followings are for test
     train_annot_path = "/home/kloping/OpenSet_MOT/data/TAO/annotations/train.json"
     with open(train_annot_path, 'r') as f:
         train_annot_dict = json.load(f)
@@ -110,8 +149,8 @@ if __name__ == "__main__":
     check_coco_tao_intersection(val_annot_dict['categories'], split='VAL')
 
     # TEST 2: Check how many classes are covered in TRAIN and how many covered in VAL
-    check_train_val_diff(train_annot_dict['categories'], val_annot_dict['categories'])
-    print("###################################")
+    # check_train_val_diff(train_annot_dict['categories'], val_annot_dict['categories'])
+    # print("###################################")
 
     # Conversion (No need)
     # convert_to_coco(annot_dict)
