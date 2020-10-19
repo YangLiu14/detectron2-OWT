a071a385d1d7337de65dec3da9a1631a07ba2cec
ga27kuh <ga27kuh@mytum.de>
2020-10-19 07:51:20 +0200

    extract annotated frames from train_set

--- eval/dataset_statistics.py
+++ eval/dataset_statistics.py
@@ -4,6 +4,77 @@ import os
 import sys
 
 
+def map_image_id2idx(annot_dict: str):
+    """
+    Map the image_id in annotatition['images'] to its index.
+    Args:
+        annot_dict: The annotation file (loaded from json)
+
+    Returns:
+        Dict
+    """
+    images = annot_dict['images']
+    res = dict()
+    for i, img in enumerate(images):
+        res[img['id']] = i
+
+    return res
+
+
+def get_annotated_sequences2():
+    root_dir = "/home/kloping/OpenSet_MOT/data/TAO/annotations/"
+    fname = "train.json"
+    with open (root_dir + fname, 'r') as f:
+        annot_dict = json.load(f)
+
+    image_id2idx = map_image_id2idx(annot_dict)
+
+    category_dict = annot_dict['categories']
+    annots = annot_dict['annotations']
+    tracks = annot_dict['tracks']
+    videos = annot_dict['videos']
+    images = annot_dict['images']
+
+
+    gt_fnames = list()
+    for ann in annots:
+        cat_id = ann['category_id']
+        idx = image_id2idx[ann['image_id']]
+        img = images[idx]
+        img_fname = img['file_name']
+        img_fname = "/".join(img_fname.split('/')[1:])  # Get rid of "train/" or "val/" at the begining
+        gt_fnames.append(img_fname)
+
+    # Write to txt
+    ArgoVerse = list()
+    BDD = list()
+    Charades = list()
+    LaSOT = list()
+    YFCC100M = list()
+    for seq in gt_fnames:
+        data_src = seq.split("/")[0]
+        if data_src == "ArgoVerse":
+            ArgoVerse.append(seq.replace(".json", ".jpg"))
+        elif data_src == "BDD":
+            BDD.append(seq.replace(".json", ".jpg"))
+        elif data_src == "Charades":
+            Charades.append(seq.replace(".json", ".jpg"))
+        elif data_src == "LaSOT":
+            LaSOT.append(seq.replace(".json", ".jpg"))
+        elif data_src == "YFCC100M":
+            YFCC100M.append(seq.replace(".json", ".jpg"))
+        else:
+            print(seq, "ignored")
+
+    # Write to corresponding txt files
+    data_src_names = ['ArgoVerse', 'BDD', 'Charades', 'LaSOT', 'YFCC100M']
+    all_list = [ArgoVerse, BDD, Charades, LaSOT, YFCC100M]
+    for name, l in zip(data_src_names, all_list):
+        with open('train_annotated_{}.txt'.format(name), 'w') as f:
+            for item in l:
+                f.write("%s\n" % item)
+
+
 def get_annotated_sequences():
     """
     Not all the sequences in TAO as annotated.
@@ -63,4 +134,4 @@ if __name__ == "__main__":
     #
     # print(num_images)
 
-    get_annotated_sequences()
+    get_annotated_sequences2()
