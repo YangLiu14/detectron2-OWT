5af6b74e12bb74c6e4c8e0de85613c540b7ae1a1
YangLiu14 <liuyang@atcremers17.in.tum.de>
2020-11-01 20:32:21 +0100

    tbc

--- run_tao.sh
+++ run_tao.sh
@@ -131,8 +131,8 @@ python eval_recall_vs_NinTracks.py --plot_output_dir /storage/slurm/liuyang/TAO_
 # NMS Post-processing
 # ==============================================================================
 python NMS_postprocessing.py --scorings "score" "bg_score" "one_minus_bg_score" "objectness" "bg_rpn_sum" "bg_rpn_product" \
-                             --nms_criterion bbox \
+                             --nms_criterion instance_mask \
                              --inputdir /storage/slurm/liuyang/TAO_eval/TAO_VAL_Proposals/Panoptic_Cas_R101_NMSoff+objectness003/json/ \
-                             --outdir "/storage/slurm/liuyang/TAO_eval/TAO_VAL_Proposals/postNMS_bbox/Panoptic_Cas_R101_NMSoff+objectness003"
+                             --outdir "/storage/slurm/liuyang/TAO_eval/TAO_VAL_Proposals/postNMS_mask/Panoptic_Cas_R101_NMSoff+objectness003"
 
 
