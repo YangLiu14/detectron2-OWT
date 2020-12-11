b59395fa09d71f51de4187723bc822407fbdfc7c
YangLiu14 <liuyang@atcremers17.in.tum.de>
2020-12-11 13:42:03 +0100

    trivials

--- run_tao.sh
+++ run_tao.sh
@@ -93,7 +93,7 @@ python gen_tao_proposals_placeholder.py --config-file ../configs/Misc/noNMS/pano
 python gen_tao_proposals_subset.py --config-file ../configs/Misc/noNMS/panoptic_fpn_R_101_dconv_cascade_gn_3x.yaml \
                          --input /storage/slurm/liuyang/data/TAO/TAO_VAL/val/ \
                          --json /storage/slurm/liuyang/TAO_eval/TAO_VAL_Proposals/subset_experiment/Panoptic_Cas_R101_NMSoff_forTracking_Embed/json/ \
-                         --video_src_name LaSOT  \
+                         --video_src_name ArgoVerse  \
                          --opts MODEL.WEIGHTS /storage/slurm/liuyang/model_weights/detectron2/Panoptic_FPN_R101/model_final_be35db.pkl
 
 # Mask RCNN
@@ -180,6 +180,6 @@ python NMS_postprocessing.py --scorings "objectness" "bg_score" "score" "one_min
 
 python NMS_postprocessing.py --scorings "objectness" "score" "one_minus_bg_score" \
                              --nms_criterion bbox \
-                             --inputdir /storage/slurm/liuyang/TAO_eval/TAO_VAL_Proposals/Panoptic_Cas_R101_NMSoff_forTracking/json/ \
-                             --outdir /storage/slurm/liuyang/TAO_eval/TAO_VAL_Proposals/Panoptic_Cas_R101_NMSoff_forTracking/boxNMS/ \
-                             --datasrc ArgoVerse
+                             --inputdir /storage/slurm/liuyang/TAO_eval/TAO_VAL_Proposals/subset_experiment/Panoptic_Cas_R101_NMSoff_forTracking_Embed/json/ \
+                             --outdir /storage/slurm/liuyang/TAO_eval/TAO_VAL_Proposals/subset_experiment/Panoptic_Cas_R101_NMSoff_forTracking_Embed/boxNMS/ \
+                             --datasrc YFCC100M
